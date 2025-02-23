5f8a6ca8665cba6cfe3a8e56123a9a12f0bb022e
Theodore Lesser <ted.a.lesser@gmail.com>
2025-02-23 13:08:15 -0500

    Update max-width & other sizings

--- src/components/SavedCardList/SavedCardList.jsx
+++ src/components/SavedCardList/SavedCardList.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./SavedCardList.css"; // Assuming you will have a separate CSS file for SavedCards
 import NewsCard from "../NewsCard/NewsCard";
 
-const SavedCardList = ({ savedArticles, onCardDelete, article, handleCardLike }) => {
+const SavedCardList = ({
+  savedArticles,
+  onCardDelete,
+  article,
+  handleCardLike,
+}) => {
   const handleDeleteClick = () => {
     onCardDelete(article); // Call the delete function passed from props
   };
@@ -10,13 +15,13 @@ const SavedCardList = ({ savedArticles, onCardDelete, article, handleCardLike })
   return (
     <div className="saved-cards">
       {savedArticles?.map((article) => (
-        <NewsCard 
-        key={article._id} 
-        article={article} 
-        onCardLike={handleCardLike} 
-        onCardDelete={onCardDelete} 
-        savedArticles={savedArticles}
-      />
+        <NewsCard
+          key={article._id}
+          article={article}
+          onCardLike={handleCardLike}
+          onCardDelete={onCardDelete}
+          savedArticles={savedArticles}
+        />
       ))}
     </div>
   );
